3fd33d8ff2e48215d0dab44245030473eda026dd
eityans <eityans@gmail.com>
2023-01-07 01:33:30 +0900

    call query

---
+++ lib/octopus_graphql.rb
@@ -0,0 +1,17 @@
+require "graphql/client"
+require "graphql/client/http"
+
+module OctopusGraphql
+  # Configure GraphQL endpoint using the basic HTTP network adapter.
+  HTTP = GraphQL::Client::HTTP.new("https://api.oejp-kraken.energy/v1/graphql/") do
+    def headers(context)
+      # Optionally set any HTTP headers
+      {   "Authorization": ENV['OCTOPUS_TOKEN']}
+    end
+  end
+
+  # Fetch latest schema on init, this will make a network request
+  Schema = GraphQL::Client.load_schema(HTTP)
+
+  Client = GraphQL::Client.new(schema: Schema, execute: HTTP)
+end
