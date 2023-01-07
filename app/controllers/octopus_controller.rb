24fe3e12e50c09bcc34be310e2b149fe3960d61b
eityans <eityans@gmail.com>
2023-01-08 01:44:46 +0900

    calculate cost

--- app/controllers/octopus_controller.rb
+++ app/controllers/octopus_controller.rb
@@ -10,6 +10,7 @@ class OctopusController < ApplicationController
               endAt
               version
               value
+              costEstimate
             }
           }
         }
@@ -18,11 +19,18 @@ class OctopusController < ApplicationController
   GRAPHQL
 
   def index
-    p OctopusGraphql::Client.query(QUERY::HalfHourlyReadings, variables: {
+    res = OctopusGraphql::Client.query(QUERY::HalfHourlyReadings, variables: {
       accountNumber: ENV['OCTOPUS_ACCOUNT_NUMBER'],
-      fromDatetime: "2022-08-14T15:00:00.000Z",
-      toDatetime: "2022-12-14T17:00:00.000Z"
+      fromDatetime: Date.yesterday.beginning_of_day.iso8601,
+      toDatetime: Date.yesterday.end_of_day.iso8601
     })
+    properties = res.original_hash.dig("data", "account", "properties")
+    electricitySupplyPoints = properties.first["electricitySupplyPoints"]
+    halfHourlyReadings = electricitySupplyPoints.first["halfHourlyReadings"]
+    @start_at = halfHourlyReadings.pluck("startAt").min.to_time.localtime.strftime("%Y/%m/%d %H:%M")
+    @end_at = halfHourlyReadings.pluck("endAt").max.to_time.localtime.strftime("%Y/%m/%d %H:%M")
+    @kwh = halfHourlyReadings.pluck("value").map(&:to_f).sum
+    @cost = halfHourlyReadings.pluck("costEstimate").map(&:to_i).sum
   end
 end
 
