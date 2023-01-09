758355c9276a02f8b610cd710380ea8b967dc6d2
eityans <eityans@gmail.com>
2023-01-09 21:13:08 +0900

    message

--- app/controllers/octopus_controller.rb
+++ app/controllers/octopus_controller.rb
@@ -19,18 +19,7 @@ class OctopusController < ApplicationController
   GRAPHQL
 
   def index
-    res = OctopusGraphql::Client.query(QUERY::HalfHourlyReadings, variables: {
-      accountNumber: ENV['OCTOPUS_ACCOUNT_NUMBER'],
-      fromDatetime: Date.yesterday.beginning_of_day.iso8601,
-      toDatetime: Date.yesterday.end_of_day.iso8601
-    })
-    properties = res.original_hash.dig("data", "account", "properties")
-    electricitySupplyPoints = properties.first["electricitySupplyPoints"]
-    halfHourlyReadings = electricitySupplyPoints.first["halfHourlyReadings"]
-    @start_at = Time.zone.parse(halfHourlyReadings.pluck("startAt").min).strftime("%Y/%m/%d %H:%M")
-    @end_at = Time.zone.parse(halfHourlyReadings.pluck("endAt").max).strftime("%Y/%m/%d %H:%M")
-    @kwh = halfHourlyReadings.pluck("value").map(&:to_f).sum
-    @cost = halfHourlyReadings.pluck("costEstimate").map(&:to_i).sum
+    @text = gen_text
     send_line_message
   end
 
@@ -39,7 +28,7 @@ class OctopusController < ApplicationController
   def send_line_message
     message = {
       type: 'text',
-      text: 'hello'
+      text: @text
     }
 
     client = Line::Bot::Client.new do |config|
@@ -48,6 +37,23 @@ class OctopusController < ApplicationController
     end
 
     client.broadcast(message)
+  end
+
+  def gen_text
+    res = OctopusGraphql::Client.query(QUERY::HalfHourlyReadings, variables: {
+      accountNumber: ENV['OCTOPUS_ACCOUNT_NUMBER'],
+      fromDatetime: Date.yesterday.beginning_of_day.iso8601,
+      toDatetime: Date.yesterday.end_of_day.iso8601
+    })
+    properties = res.original_hash.dig("data", "account", "properties")
+    electricitySupplyPoints = properties.first["electricitySupplyPoints"]
+    halfHourlyReadings = electricitySupplyPoints.first["halfHourlyReadings"]
+    start_at = Time.zone.parse(halfHourlyReadings.pluck("startAt").min).strftime("%Y/%m/%d %H:%M")
+    end_at = Time.zone.parse(halfHourlyReadings.pluck("endAt").max).strftime("%Y/%m/%d %H:%M")
+    kwh = halfHourlyReadings.pluck("value").map(&:to_f).sum
+    cost = halfHourlyReadings.pluck("costEstimate").map(&:to_i).sum
+
+    "#{start_at}から#{end_at}までの電力消費量は#{kwh}kWhで#{cost}円です。"
   end
 end
 
